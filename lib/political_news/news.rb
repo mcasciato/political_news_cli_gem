419fdc1cd78ded29675b6c6b6004724f93b05a6c
mcasciato <michael.casciato@icloud.com>
2016-05-17 16:08:14 -0400

    added buzzfeed politics

--- lib/political_news/news.rb
+++ lib/political_news/news.rb
@@ -3,7 +3,7 @@ class PoliticalNews::News
   attr_accessor :name, :author, :summary, :url, :source
 
   def self.today
-  #Scrape New York Times, Washington Post, and TIME (change to Politico?)and return newest political articles
+  #Scrape New York Times, Washington Post, and TIME (change to Politico? other?)and return newest political articles
     self.scrape_articles
   end
 
@@ -12,13 +12,13 @@ class PoliticalNews::News
 
     articles << self.scrape_nytimes
     articles << self.scrape_washpost
-    articles << self.scrape_politico
+    articles << self.scrape_buzzfeed_politics
 
     # Go to New York Times, find article
     # Extract properties
     # instantiate article
 
-    # Do same for washington post and TIME (change to Politico?)
+    # Do same for washington post and TIME (change to Politico? other?)
      articles 
   end
 
@@ -46,16 +46,15 @@ class PoliticalNews::News
     article 
   end
 
-  def self.scrape_politico
-     doc = Nokogiri::HTML(open("https://www.washingtonpost.com/politics/?nid=menu_nav_politics"))
+  def self.scrape_buzzfeed_politics
+     doc = Nokogiri::HTML(open("https://www.buzzfeed.com/politics"))
     
     article = self.new
-    binding.pry
-    # article.name = doc.search("h2").first.text.strip
-    # article.author = doc.search("h6.byline").first.text.strip
-    # article.summary = doc.search("div.ledeStory p.summary").first.text.strip
-    # article.url = doc.search("h2 a").first.attr("href").strip
-    # article.source = doc.search("div #branding a img").first["alt"]
+    article.name = doc.search("div.lede__body h2.lede__title").first.text.strip
+    article.author = doc.search("div.small-meta__item").first.text.strip
+    article.summary = doc.search("p.lede__kicker").first.text.strip
+    article.url = "https://www.buzzfeed.com/politics" + doc.search("div.lede__body h2.lede__title a").first.attr("href").strip
+    article.source = doc.search("div #logo a").first.text
     article
   end
 end
